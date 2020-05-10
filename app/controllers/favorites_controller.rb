2020926fc9a66e51911be53a2f8fd15e4c520be1
megumi.hirao <megkomegi@gmail.com>
2020-05-10 06:40:08 +0000

    redirect_revision

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,13 +4,13 @@ class FavoritesController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.favorite(micropost)
     flash[:success] = '投稿をお気に入りにしました。'
-    redirect_to root_url
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.unfavorite(micropost)
     flash[:success] = '投稿をお気に入りから解除しました。'
-    redirect_to root_url
+    redirect_back(fallback_location: root_path)
   end
 end
